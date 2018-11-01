145779ba0b20a833879b9495a53b41304f7b4daa
Carlos Baez <cebaezt@ut.edu.co>
2018-11-01 18:42:20 -0500

    se sigue ajustando el diseño

--- src/app/ingresar-vehiculo/ingresar-vehiculo.component.ts
+++ src/app/ingresar-vehiculo/ingresar-vehiculo.component.ts
@@ -1,70 +1,70 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Vehiculo } from '../models/vehiculo';
 import { VehiculoService } from '../vehiculo.service';
-import {NgForm} from '@angular/forms';
+import { NgForm } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 
 @Component({
-  selector: 'app-ingresar-vehiculo',
-  templateUrl: './ingresar-vehiculo.component.html',
-  styleUrls: ['./ingresar-vehiculo.component.css']
+    selector: 'app-ingresar-vehiculo',
+    templateUrl: './ingresar-vehiculo.component.html',
+    styleUrls: ['./ingresar-vehiculo.component.css']
 })
 
 export class IngresarVehiculoComponent implements OnInit {
 
-  @Output() header = new EventEmitter();
+    @Output() header = new EventEmitter();
 
-  vehiculoModel = new Vehiculo();
-  titulo: String;
-  mensaje: String;
-  mostrarMensaje: boolean;
-  mensajeExitoso: boolean = false;
-  tipoVehiculoMoto : number = 1;
-  tipoVehiculoCarro : number = 2;
-  recargar : number = 1;
+    vehiculoModel = new Vehiculo();
+    titulo: String;
+    mensaje: String;
+    tipoVehiculoMoto: number = 1;
+    tipoVehiculoCarro: number = 2;
+    recargar: number = 1;
+    procesoExitoso: boolean;
 
-  constructor(
-    private vehiculoService: VehiculoService
-  ) {
-    
-  }
+    constructor(
+        private vehiculoService: VehiculoService,
+        private modalService: NgbModal
+    ) {
 
-  ngOnInit() {
-  }
+    }
 
-  onSubmit(form: NgForm)
-  {
-    this.vehiculoService.ingresarVehiculo(this.vehiculoModel).subscribe(
-      res => {
-        this.titulo = "Exitoso";
-        this.mensaje = "El Vehículo "+ this.vehiculoModel.placa +" ingresó al parqueadero exitosamente";
-        this.mensajeExitoso = true;
-        form.resetForm();
-        this.recargarListadoParqueos();
-        this.mostrarMensaje = true;      },
-      err => {
-        console.log(err);
-        this.titulo = "Atención";
-        if(err.status == 0 || err.status == 500)
-        {
-          this.mensaje = "Error de conexión, por favor contacte al administrador";
-        }
-        else{
-          this.mensaje = err.text();
-        }
-        this.mensajeExitoso = false;
-        this.mostrarMensaje = true;
-      }
-    )
-  }
+    ngOnInit() {
+        
+    }
 
-  ocultarMensaje()
-  {
-    this.mostrarMensaje = false;
-  }
+    onSubmit(form: NgForm, content) {
+        this.vehiculoService.ingresarVehiculo(this.vehiculoModel).subscribe(
+            res => {
+                this.titulo = "Fantástico";
+                this.mensaje = "El Vehículo " + this.vehiculoModel.placa + " ingresó al parqueadero exitosamente";
+                this.procesoExitoso = true;
+                this.mostrarModalConfirmacion(content);
+                form.resetForm();
+                this.recargarListadoParqueos();
+            },
+            err => {
+                console.log(err);
+                this.titulo = "Error";
+                if (err.status == 0 || err.status == 500) {
+                    this.mensaje = "Error de conexión, por favor contacte al administrador";
+                }
+                else {
+                    this.mensaje = err.text();
+                }
+                this.procesoExitoso = false;
+                this.mostrarModalConfirmacion(content);
+            }
+        )
+    }
 
-  recargarListadoParqueos()
-  {
-    this.header.emit(this.recargar++);
-  }
+    recargarListadoParqueos() {
+        this.header.emit(this.recargar++);
+    }
+
+    mostrarModalConfirmacion(content)
+    {
+        this.modalService.open(content, { size: 'lg', centered: true  });
+    }
 }
