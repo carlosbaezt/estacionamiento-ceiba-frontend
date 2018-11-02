5e19cf40adbe3c46c05787d8a90f0c33a14fb465
Carlos Eduardo Baez - Ceiba Software <carlos.baez@ceiba.com.co>
2018-11-02 11:34:39 -0500

    se ajusta el diseño con el logo de ceiba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Estacionamiento - ADN Ceiba';
+  title = 'Estacionamiento - ADN';
   mensaje = '';
 
   recargarListadoParqueos(mensaje)
