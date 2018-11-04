e919d8d6bbf0a100776d3a8cfdcdc8290b7822bf
Carlos Baez <cebaezt@ut.edu.co>
2018-11-04 17:16:45 -0500

    se anade la URL de produccion v2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "http://35.243.194.8:8080/estacionamientoApi-0.0.1-SNAPSHOT/vigilante/retirarVehiculo/ACV123"
+  apiUrl: "http://35.243.194.8:8080/estacionamientoApi-0.0.1-SNAPSHOT/vigilante/"
 };
